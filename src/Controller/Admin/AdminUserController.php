d13b7ba00bd257840001c66efa348d99cc58b5a0
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-26 15:30:48 +0200

    create user

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminUserController extends AbstractController
@@ -27,5 +32,38 @@ class AdminUserController extends AbstractController
         $user = $userRepository->find($id);
 
         return $this->render("admin/user_show.html.twig", ['user' => $user]);
+    }
+
+    /**
+     * @Route("admin/create/user", name="admin_create_user")
+     */
+    public function adminCreateUser(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        UserPasswordHasherInterface $userPasswordHasherInterface
+    ) {
+        $user = new User();
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setRoles(["ROLE_ADMIN"]);
+            $user->setDateEnregistrement(new \DateTime("NOW"));
+
+            $plainPassword = $userForm->get('password')->getData();
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("admin_list_user");
+        }
+
+        return $this->render('admin/user_form.html.twig', ['userForm' => $userForm->createView()]);
     }
 }
